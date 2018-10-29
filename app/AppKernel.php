9525642d7814feca1069d235ef38f565afb4f67d
ffouillet <felix.fouillet@gmail.com>
2018-10-29 16:35:55 +0100

    Created CoreBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
             new BileMo\AppBundle\BileMoAppBundle(),
             new Fx\UserBundle\FxUserBundle(),
+            new CoreBundle\CoreBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
