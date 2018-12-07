84851e9298fc6a0efac370f4bf69c2c01e245ee7
ffouillet <felix.fouillet@gmail.com>
2018-12-07 09:25:09 +0100

    Added another Customer in fixtures and users related to in order to test the project.

--- src/AppBundle/DataFixtures/DemoCustomerAndUsersFixture.php
+++ src/AppBundle/DataFixtures/DemoCustomerAndUsersFixture.php
@@ -14,25 +14,45 @@ class DemoCustomerAndUsersFixture extends Fixture
     public function load(ObjectManager $manager)
     {
         $demoCustomer = new Customer();
-        $demoCustomer->setCompanyName('BileMo\'s Demo Customer Company');
-        $demoCustomer->setUsername('bileMoDemoCustomer');
-        $demoCustomer->setPlainPassword('bileMoDemoCustomerPassword');
-        $demoCustomer->setEmail('demoCustomer@demoCustomerMobileShop.com');
-        $demoCustomer->setWebsiteUrl('http://www.bileMoDemoCustomerMobileShop.com');
+        $demoCustomer->setCompanyName('Blue Mobile Shop');
+        $demoCustomer->setUsername('blueMobileShop');
+        $demoCustomer->setPlainPassword('blueMobilePassword');
+        $demoCustomer->setEmail('dev@blueMobileShop.com');
+        $demoCustomer->setWebsiteUrl('http://www.blueMobileShop.com');
 
         $manager->persist($demoCustomer);
 
+        $otherDemoCustomer = new Customer();
+        $otherDemoCustomer->setCompanyName('Red Mobile Shop');
+        $otherDemoCustomer->setUsername('redMobileShop');
+        $otherDemoCustomer->setPlainPassword('redMobilePassword');
+        $otherDemoCustomer->setEmail('dev@redMobileShop.com');
+        $otherDemoCustomer->setWebsiteUrl('http://www.redMobileShop.com');
+
+        $manager->persist($otherDemoCustomer);
+
         // Generate demoCustomer's users.
-        for($i = 0; $i <= 10; $i++) {
+        for($i = 1; $i <= 20; $i++) {
 
             $demoUser = new User();
-            $demoUser->setUsername('demoUser'.$i);
-            $demoUser->setPlainPassword('demoPassword'.$i);
-            $demoUser->setEmail('demoMail'.$i.'@demo.com');
-            $demoUser->setFirstName('DemoUser'.$i.'FirstName');
-            $demoUser->setLastName('DemoUser'.$i.'LastName');
-            $demoUser->setCreatedAt(new \DateTime());
-            $demoUser->setCustomer($demoCustomer);
+
+            if ($i < 11) {
+                $demoUser->setUsername('blue-demoUser-'.$i);
+                $demoUser->setPlainPassword('demoPassword'.$i);
+                $demoUser->setEmail('demoMail'.$i.'@blueMobileShop.com');
+                $demoUser->setFirstName('BlueDemoUser-'.$i.'-FirstName');
+                $demoUser->setLastName('BlueDemoUser-'.$i.'-LastName');
+                $demoUser->setCreatedAt(new \DateTime());
+                $demoUser->setCustomer($demoCustomer);
+            } else {
+                $demoUser->setUsername('red-demoUser-'.$i);
+                $demoUser->setPlainPassword('demoPassword'.$i);
+                $demoUser->setEmail('demoMail'.$i.'@redMobileShop.com');
+                $demoUser->setFirstName('RedDemoUser'.$i.'FirstName');
+                $demoUser->setLastName('RedDemoUser'.$i.'LastName');
+                $demoUser->setCreatedAt(new \DateTime());
+                $demoUser->setCustomer($otherDemoCustomer);
+            }
 
             $manager->persist($demoUser);
         }
